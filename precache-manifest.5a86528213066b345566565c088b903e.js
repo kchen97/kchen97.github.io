82fe84df7034dc583bdb27762570fd5ec4baa5c2
Korman Chen <kc@kormans-mbp.attlocal.net>
2020-07-05 23:03:29 -0700

    Updates

---
+++ precache-manifest.5a86528213066b345566565c088b903e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2264557dc6d571533860ec2fca3e2e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "385084e17d96a951e22e",
+    "url": "/static/css/main.f9fc6514.chunk.css"
+  },
+  {
+    "revision": "504a8220151213c2e0b4",
+    "url": "/static/js/2.f78a8d70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f78a8d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385084e17d96a951e22e",
+    "url": "/static/js/main.193770ca.chunk.js"
+  },
+  {
+    "revision": "39076724af41f38a771f",
+    "url": "/static/js/runtime-main.0d7fe97c.js"
+  }
+]);
