7cc8eeac02bad33cd83301e79e72ebc88a183382
Korman Chen <kc@Kormans-MacBook-Pro.local>
2020-07-08 16:28:28 -0700

    Updates

--- precache-manifest.e723f20f267d72468acc921bb1ec9482.js
+++ precache-manifest.e723f20f267d72468acc921bb1ec9482.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f29dfac83fafdc650cfb6b25507151c",
+    "revision": "6feed98edda61c170e0669419643b2d5",
     "url": "/index.html"
   },
   {
-    "revision": "c1072c14ccc0d8bfcfbc",
+    "revision": "3973fcbbafcc789f083d",
     "url": "/static/css/main.cebcde4d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.88951553.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c1072c14ccc0d8bfcfbc",
-    "url": "/static/js/main.8765458e.chunk.js"
+    "revision": "3973fcbbafcc789f083d",
+    "url": "/static/js/main.ba72a871.chunk.js"
   },
   {
     "revision": "39076724af41f38a771f",
